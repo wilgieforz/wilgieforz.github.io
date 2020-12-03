88e88d99797d020fc4d7146565f0dc3a12802fbd
Egor <wilgieforz@mail.ru>
2020-12-04 02:25:27 +0700

    add UberProject

---
+++ UberProject/js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+    window.addEventListener('DOMContentLoaded', () => {
+        const menu = document.querySelector('.menu'),
+            menuItem = document.querySelectorAll('.menu_item'),
+            hamburger = document.querySelector('.hamburger');
+
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        });
+
+        menuItem.forEach(item => {
+            item.addEventListener('click', () => {
+                hamburger.classList.toggle('hamburger_active');
+                menu.classList.toggle('menu_active');
+            })
+        })
+    })
+
+    //Smooth scroll and pageup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 800) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    new WOW().init();
+});
